7a979c187359a0c63a80673e05c77546c02f17dd
Nicolas bataille <63408806+NicolasBataille@users.noreply.github.com>
2023-11-09 20:59:14 +0100

    Added BIO tagging generation and application in text

--- baseline/NER.py
+++ baseline/NER.py
@@ -1,6 +1,13 @@
-from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import (
+    AutoTokenizer,
+    AutoModelForTokenClassification,
+    # CamembertModel,
+    # CamembertTokenizer,
+)
 import json
 from transformers import pipeline
+
+# import torch
 
 
 def read_file(file_path: str):
@@ -8,12 +15,40 @@ def read_file(file_path: str):
         return f.read()
 
 
-def get_entities(text: str):
+def write_json_file(file_path: str, data: list):
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def remove_duplicates_by_word(data):
+    """
+    Removes duplicate entities based on the 'word' attribute.
+
+    Args:
+        data (list): A list of dictionaries representing the entities.
+
+    Returns:
+        list: A list of dictionaries with duplicate entities removed.
+    """
+    unique_dicts = []
+    words = set()
+
+    for entity in data:
+        word = entity["word"]
+        if word not in words:
+            unique_dicts.append(entity)
+            words.add(word)
+
+    return unique_dicts
+
+
+def get_entities(text: str, keep_duplicates: bool = False):
     """
     Extracts named entities from the given text.
 
     Args:
         text (str): The input text.
+        keep_duplicates (bool, optional): Whether to keep duplicate entities. Defaults to False.
 
     Returns:
         list: A list of dictionaries representing the named entities. Each dictionary contains the keys 'entity_group',
@@ -38,13 +73,64 @@ def get_entities(text: str):
         for entity in raw_result
     ]
 
+    # Only keep the entities that are persons
     filtered_results = [
         entity for entity in raw_result if entity["entity_group"] == "PER"
     ]
 
+    # Remove duplicates
+    if keep_duplicates:
+        filtered_results = remove_duplicates_by_word(filtered_results)
+
     return filtered_results
 
 
-def write_json_file(file_path: str, data: list):
-    with open(file_path, "w") as f:
-        json.dump(data, f, indent=4)
+def add_bio_tags(entities):
+    """
+    Adds BIO tags to the named entities.
+
+    Args:
+        entities (list): A list of dictionaries representing the named entities. Each dictionary contains the keys 'entity_group',
+                          'word', 'start', and 'end'.
+
+    Returns:
+        list: A list of dictionaries representing the named entities with the added 'bio_tag' key.
+    """
+    for entity in entities:
+        word = entity["word"]
+        bio_tag = ""
+        for i, token in enumerate(word.split()):
+            if i == 0:
+                bio_tag += token + "<B-PER>"
+            else:
+                bio_tag += " " + token + "<I-PER>"
+        entity["bio_tag"] = bio_tag
+    return entities
+
+
+def tag_text(text: str, entities: list):
+    """
+    Tags the text with BIO tags based on the given entities.
+
+    Args:
+        text (str): The input text.
+        entities (list): A list of dictionaries representing the named entities. Each dictionary contains the keys 'entity_group',
+                          'word', 'start', 'end', and 'bio_tag'.
+
+    Returns:
+        str: The tagged text.
+    """
+    tagged_text = ""
+    start = 0
+    for entity in entities:
+        tagged_text += text[start : entity["start"] + 1] + entity["bio_tag"]
+        start = entity["end"] + 1
+    tagged_text += text[start:]
+
+    return tagged_text
+
+
+# def tokenize(entities: list):
+#     model_name = 'camembert-base'
+#     tokenizer = CamembertTokenizer.from_pretrained(model_name)
+#     model = CamembertModel.from_pretrained(model_name)
