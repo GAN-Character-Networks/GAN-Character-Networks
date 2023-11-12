b355edb7e4d2444d79602c00a307b4e432b19c3c
gabrieldesbouis <gabriel.desbouis@alumni.univ-avignon.fr>
2023-11-12 18:24:34 +0100

    Adds Tokenizer and count them in Kaggle's folder

---
+++ vroom/Tokenizer.py
@@ -0,0 +1,16 @@
+import tiktoken
+
+class Tokenizer:
+    def __init__(self, model: str = "gpt-4"):
+        self.encoding = tiktoken.encoding_for_model(model)
+
+
+    def tokenize(self, text: str):
+        self.tokenized = True
+        self.tokens = self.encoding.encode(text)
+        return self.tokens
+
+    def count_tokens(self):
+        if not self.tokenized:
+            raise Exception("You must tokenize the text before counting the tokens.")
+        return len(self.tokens)
