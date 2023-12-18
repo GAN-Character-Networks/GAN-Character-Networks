dab2f4edb2e4f6a5170e5d315989e5b415de3064
gabrieldesbouis <gabriel.desbouis@alumni.univ-avignon.fr>
2023-12-18 16:27:01 +0100

    Adds support to new structure of data from baseline

--- examples/baseline.py
+++ examples/baseline.py
@@ -1,9 +1,15 @@
 from vroom.baseline import get_cooccurences_from_text
+from vroom.GraphManager import GraphManager
+
 
 def baseline(text):
     return get_cooccurences_from_text(text)
 
-if __name__ == "__main__":
-    path = "data/kaggle/les_cavernes_d_acier/chapter_1.txt.preprocessed"
 
-    print(baseline(path))
+if __name__ == "__main__":
+    path = "../data/kaggle/les_cavernes_d_acier/chapter_1.txt.preprocessed"
+
+    graphManager = GraphManager()
+    coocurrences = baseline(path)
+    graphManager.add_cooccurrences(coocurrences)
+    graphManager.save_graph_to_graphml("chapter_1.graphml")
--- vroom/GraphManager.py
+++ vroom/GraphManager.py
@@ -9,12 +9,13 @@ class GraphManager:
 
     Exemple d'utilisation:
     graph_manager = GraphManager(weighted=True)
-    cooccurrences = [("Sheldon", "Arthur"), ("Arthur", "Bilal"), ("Sheldon", "Arthur")]
+    cooccurrences = [(["Sheldon", "Harry"], ["Arthur", "Amalvy"]), (["Arthur", "Amalvy", ["Bilal", "Marzougue"]), (["Sheldon", "Harry"], ["Arthur", "Amalvy"])]
     graph_manager.add_cooccurrences(cooccurrences)
+    graphml = graph_manager.generate_graph()
     graph_manager.save_graph_to_graphml("cooccurrence_graph.graphml")
     """
 
-    def __init__(self, weighted=False):
+    def __init__(self, weighted: bool = False):
         """
         Initialise un nouveau gestionnaire de graphe.
 
@@ -23,28 +24,48 @@ class GraphManager:
         self.graph = nx.Graph()
         self.weighted = weighted
 
-    def add_cooccurrences(self, cooccurrences):
+    def add_cooccurrences(
+        self, cooccurrences: list[tuple[list[str], list[str]]]
+    ) -> None:
         """
         Ajoute ou met à jour les cooccurrences dans le graphe.
 
         Si weighted est True, les arêtes sont pondérées par le nombre de cooccurrences.
         Sinon, toutes les arêtes ont le même poids (non pondéré).
 
-        :param cooccurrences: Liste de tuples représentant les cooccurrences.
+        :param cooccurrences: Liste de tuples de listes contenant les alias des personnages impliqués dans une cooccurrence.
         """
+        aliases = {}
         for cooccurrence in cooccurrences:
+            group_1, group_2 = cooccurrence
+            aliases[group_1[0]] = group_1
+            aliases[group_2[0]] = group_2
+
+            cooccurrence = tuple([group_1[0], group_2[0]])
             if self.weighted:
                 if self.graph.has_edge(*cooccurrence):
                     # Augmenter le poids si l'arête existe déjà
                     self.graph[cooccurrence[0]][cooccurrence[1]]["weight"] += 1
                 else:
                     # Ajouter une nouvelle arête avec un poids initial
-                    self.graph.add_edge(
-                        cooccurrence[0], cooccurrence[1], weight=1
-                    )
+                    self.graph.add_edge(*cooccurrence, weight=1)
             else:
                 # Ajouter une arête sans poids
                 self.graph.add_edge(*cooccurrence)
+
+            # Ajout des alias des personnages impliqués dans la cooccurrence
+            for alias in cooccurrence:
+                self.graph.nodes[alias]["names"] = ";".join(aliases[alias])
+
+        print(self.graph)
+
+    def generate_graph(self) -> nx.graphml:
+        """
+        Génère le graphe à partir des cooccurrences ajoutées.
+
+        :return: Le graphe généré.
+        """
+        return nx.generate_graphml(self.graph)
 
     def save_graph_to_graphml(self, filename):
         """
