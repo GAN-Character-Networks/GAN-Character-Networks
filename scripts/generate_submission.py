16a5336887e14ad9c6e1f82ce87ebc4c265e6fa3
Adel Moumen <adelmoumen.pro@gmail.com>
2023-12-21 23:32:22 +0100

    update submission

--- scripts/generate_submission.py
+++ scripts/generate_submission.py
@@ -1,7 +1,7 @@
 from vroom.baseline import get_cooccurences_with_aliases
 from vroom.GraphManager import GraphManager
 import pandas as pd
-
+from tqdm import tqdm
 """
 ID : un ID unique identifiant le chapitre correspondant au graphe fourni. L'id est de la forme {code du livre}{numéro du chapitre}. Les numéros des chapitres démarrent à 0. Les codes de livres sont paf et lca (pour Prélude à Fondation et Les Cavernes d'Acier, respectivement). Ainsi, le graphe du premier chapitre de Prélude à Fondation a pour ID paf0, et celui du dernier chapitre des Cavernes d'Acier a pour ID lca17.
 graphml :: le graphe du chapitre identifié par la colonne ID, au format graphml. Chaque noeud peut être n'importe quelle chaîne de caractères, mais un attribut names doit être présent contenant les noms du personnages apparaissant durant le chapitre. Ces noms doivent être séparés par des points-virgules (exemple: Hari;Hari Seldon)
@@ -20,8 +20,8 @@ def generate_submission():
 
     df_dict = {"ID": [], "graphml": []}
 
-    for chapters, book_code in books:
-        for chapter in chapters:
+    for chapters, book_code in tqdm(books):
+        for chapter in tqdm(chapters):
             if book_code == "paf":
                 path = f"data/kaggle/prelude_a_fondation/chapter_{chapter}.txt.preprocessed"
             else:
@@ -30,7 +30,7 @@ def generate_submission():
             graph_manager = GraphManager()
             coocurrences = get_cooccurences_with_aliases(path)
             graph_manager.add_cooccurrences(coocurrences)
-            df_dict["ID"].append(f"{book_code}{chapter}")
+            df_dict["ID"].append(f"{book_code}{chapter-1}")
             df_dict["graphml"].append("".join(graph_manager.generate_graph()))
 
     df = pd.DataFrame(df_dict)
