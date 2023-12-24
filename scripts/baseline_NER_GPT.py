6b192cc2fc2db045a25eb231800a4dc2f48b9ea5
Adel Moumen <adelmoumen.pro@gmail.com>
2023-12-24 15:20:00 +0100

    update

--- scripts/baseline_NER_GPT.py
+++ scripts/baseline_NER_GPT.py
@@ -2,32 +2,9 @@ import re
 from vroom.NER import *
 import os
 
-def separate_words(text):
-    # Use regular expression to find words and punctuation, including special words like <PER> and </PERS>
-    words = re.findall(r'\b\w+\b|[.,;!?<>/]+|<PER>|</PER>', text)
-    return words
-
-def merge_special_words(word_list):
-    merged_list = []
-    current_word = ""
-
-    for word in word_list:
-        if word in ['<', 'PER', '</', '>']:
-            current_word += word
-        else:
-            if current_word:
-                merged_list.append(current_word)
-                current_word = ""
-            merged_list.append(word)
-
-    if current_word:
-        merged_list.append(current_word)
-
-    return merged_list
-
 unlabeled_chapter = os.path.join("data", "test_set", "prelude_a_fondation", "chapter_1.unlabeled")
 
-entities, chunks = get_entities_from_file(unlabeled_chapter, device="cuda")
+entities, chunks = get_entities_from_file(unlabeled_chapter)
 
 all_entities_names = []
 for chunk in entities: 
