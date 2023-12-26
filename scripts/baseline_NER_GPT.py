22a3fe36318cd971824388404cd42a314bd61332
gabrieldesbouis <gabriel.desbouis@alumni.univ-avignon.fr>
2023-12-26 03:46:09 +0100

    Fixes the baseline NER gpt. Adds some improvements

--- scripts/baseline_NER_GPT.py
+++ scripts/baseline_NER_GPT.py
@@ -11,7 +11,8 @@ import pandas as pd
 from tqdm import tqdm
 
 OPENAI_API_KEY="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"
-
+total_tokens_sent = 0
+total_tokens_received = 0
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 model = "gpt-3.5-turbo-1106" # gpt-4-1106-preview
@@ -48,18 +49,18 @@ def cooccurences_from_gpt_json(text, entities):
                             if any(abs(pos1 - pos2) <= 25 for pos1 in alias_positions[alias1] for pos2 in alias_positions[alias2]):
                                 # Ajouter la paire de listes d'alias à la liste de cooccurrences
                                 cooccurrences.append((aliases1['aliases'], aliases2['aliases']))
-                                print("Cooccurrence trouvée : ", (aliases1['aliases'], aliases2['aliases'], "A l'indice : ", tokens.index(alias1)))
                                 break  # Arrêtez de chercher d'autres occurrences pour cette paire
     return cooccurrences
 
 def build_entities_with_gpt(text, len_chunk):
     # Appel à l'API de GPT-3 pour générer les alias
     system_prompt = """
-    Tu es un expert dans l'étude des personnages dans les romans. J'ai un texte annoté par un modèle d'NER, et je n'ai gardé que les entités qu'il a estimé être des personnages. Elles sont situées entre les balises <PER></PER>. J'ai besoin qu'à partir de ce texte, tu me construises un JSON qui compile tous les personnages et leurs alias associé. Tu dois le faire comme dans cet exemple : 
+    C'est un travail capital que je te demande de faire, c'est un enjeu énorme pour ma carrière. Si tu fais un excellent travail, je te donnerai un pourboire.
+    Tu es un expert dans l'étude des personnages dans les romans. J'ai un texte annoté par un modèle d'NER, et je n'ai gardé que les entités qu'il a estimé être des personnages. Elles sont situées entre les balises <PER></PER>. J'ai besoin qu'à partir de ce texte, tu me construises un JSON qui contient tous les personnages et leurs alias associé. Il se peut que certaines entités soit marquées comme des personnages mais ne le soient pas vraiment, elles peuvent faire référence à des lieux ou des expressions par exemple, tu ne dois pas les ajouter au JSON. Tu dois le faire comme dans cet exemple : 
 
     Input : 
     <start>
-    Mathématicien  <PER> CLÉON Ier </PER>  — ... dernier  <PER> Empereur </PER>  galactique de la dynastie Entun. Né en l'an 11988 de l'Ère Galactique, la même année que  <PER> Hari  Seldon  </PER> . (On pense que la date de naissance de  <PER> Seldon </PER> , que certains estiment douteuse, aurait pu être « ajustée » pour coïncider avec celle de  <PER> Cléon </PER>  que  <PER> Seldon </PER>
+    Mathématicien  <PER> CLÉON Ier </PER>  — ... dernier  <PER> Empereur </PER>  galactique de la dynastie Entun. Né en l'an 11988 de l'Ère Galactique, la même année que  <PER> Hari  Seldon  </PER> . (On pense que la date de naissance de  <PER> Seldon </PER> , que certains estiment douteuse, aurait pu être « ajustée » pour coïncider avec celle de  <PER> Cléon </PER>  que  <PER> Seldon </PER> avait trouvé sur <PER> Trantor </PER> en 12020.)
     <end>
 
     Output : 
@@ -85,7 +86,7 @@ def build_entities_with_gpt(text, len_chunk):
 
     Voici l'input :
     """
-    len_chunk = 5000
+    len_chunk = 15000
     chunks = chunk_text(text, len_chunk)
 
     json_entities = []
@@ -103,9 +104,13 @@ def build_entities_with_gpt(text, len_chunk):
             response_format={ "type": "json_object" },
             )
 
-        json_entities.append(json.dumps(response.choices[0].message.content, indent=4, ensure_ascii=False))
-        print("Tokens sent : ", len(tokenizer.tokenize(chunk))+len_prompt)
-        print("Tokens received : ", len(tokenizer.tokenize(response.choices[0].message.content)))
+        json_entities.append(response.choices[0].message.content)
+        tokens_sent = len(tokenizer.tokenize(chunk))+len_prompt
+        print("Tokens sent : ", tokens_sent)
+        tokens_received = len(tokenizer.tokenize(response.choices[0].message.content))
+        print("Tokens received : ", tokens_received)
+        total_tokens_sent += tokens_sent
+        total_tokens_received += tokens_received
 
     return json_entities
 
@@ -115,10 +120,16 @@ def compile_json(json_entities):
     """
     # Dictionnaire global pour le résultat fusionné
     merged_entities = {}
+
+    # Vérifier s'il n'y a pas plus de deux sauts de ligne consécutifs ((\n){2,}) si oui, les remplacer par un seul
+    json_entities = [re.sub(r"((\n){2,})", "\n", json_entity) for json_entity in json_entities]
     for entity_str in json_entities:
         # Convertir la chaîne JSON en dictionnaire
-        entity_dict = json.loads(entity_str)
-        print("entity_dict : ", entity_dict)
+        try:
+            entity_dict = json.loads(entity_str)
+        except:
+            print("entity_str mal formaté : ", entity_str)
+            continue
         for key, value in entity_dict.items():
             if key not in merged_entities:
                 # Si la clé n'existe pas, l'ajouter au dictionnaire global
@@ -136,6 +147,7 @@ def compile_json(json_entities):
 
 def filter_entities(all_entities):
     filtering_prompt = """
+    C'est un travail capital que je te demande de faire, c'est un enjeu énorme pour ma carrière. Si tu fais un excellent travail, je te donnerai un pourboire.
     Tu es un expert dans l'étude des personnages dans les romans. J'ai un texte annoté par un modèle d'NER, et je n'ai gardé que les entités qu'il a estimé être des personnages.
     Nous avons eu ce JSON comme résultat mais il se peut qu'il y ait des doublons. Tu dois donc les supprimer. Si tu vois qu'un personnage a un alias qui est aussi un personnage, privilégie l'alias au personnage. Tu as stricte interdiction d'inventer des alias ou des personnages.
 
@@ -202,10 +214,12 @@ def filter_entities(all_entities):
         response_format={ "type": "json_object" },
         )
 
-    print("Tokens sent : ", len(tokenizer.tokenize(json.dumps(all_entities, indent=4, ensure_ascii=False)))+len_prompt)
-    print("Tokens received : ", len(tokenizer.tokenize(response.choices[0].message.content)))
-    print("Text received : ", response.choices[0].message.content)
-
+    tokens_sent = len(tokenizer.tokenize(json.dumps(all_entities, indent=4, ensure_ascii=False)))+len_prompt
+    print("Tokens sent : ", tokens_sent)
+    tokens_received = len(tokenizer.tokenize(response.choices[0].message.content))
+    print("Tokens received : ", tokens_received)
+    total_tokens_sent += tokens_sent
+    total_tokens_received += tokens_received
     return json.loads(response.choices[0].message.content)
 
 
@@ -234,7 +248,6 @@ def generate_submission():
             with open(path, "r") as f:
                 text = f.read()
             entities = build_entities_with_gpt(text, 5000)
-            print("Entities : ", entities)
             entities = compile_json(entities)
             entities = filter_entities(entities)
             coocurrences = cooccurences_from_gpt_json(text, entities)
@@ -251,3 +264,4 @@ def generate_submission():
 
 if __name__ == "__main__":
     generate_submission()
+    print("Prix de la submission : ", total_tokens_sent/1000*0.001 + total_tokens_received/1000*0.002, " $")
