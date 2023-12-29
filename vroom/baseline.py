8d2025dd9ab1daaa7763980a983367145523e234
Adel Moumen <adelmoumen.pro@gmail.com>
2023-12-29 15:44:46 +0100

    baseline

--- vroom/baseline.py
+++ vroom/baseline.py
@@ -87,19 +87,11 @@ def get_cooccurences_with_aliases(path: str, logger: JSONLogger = None):
     entities_unfold = [entity for sublist in entities for entity in sublist]
     aliases = get_aliases_fuzzy_partial_token(entities_unfold, 99)
 
-    if logger is not None: 
-        saves = {}
-        for i, (chunk, entity) in enumerate(zip(chunks, entities)):
-            saves[f"chunk_{i}"] = {
-                "chunk": chunk,
-                "entities": entity,
-            }
-        saves["entities"] = list(set([entity["word"] for entity in entities_unfold]))
-        saves["aliases"] = aliases
-        saves["cooccurences"] = cooccurences
-        logger(saves)
+
 
     cooccurences_aliases = find_cooccurences_aliases(cooccurences, aliases)
+    print("cooccurences_aliases: ", cooccurences_aliases)
+    exit()
     return cooccurences_aliases
 
 
@@ -117,10 +109,10 @@ def get_cooccurences_with_aliases_and_gpt(path: str, logger: JSONLogger = None):
         list: A list of tuples representing the interactions between entities in the text.
     """
     system_prompt = r"""
-Tu es un expert dans la résolution de références de personnes issus de livres de science fiction.
+Tu es un expert dans les livres "La Fondation" de Isaac Asimov.
 Ton but est d'à partir d'une liste de personnes de déterminer qui est qui en faisant un regroupement. Chaque regroupement représente une personne avec toutes ses références.
 
-Pour réaliser ce regroupement, base toi sur la sémantique des mots, notamment le genre, les titres, les ressemblances typographiques, etc. Fait  attention, certains noms ne sont pas des personnes mais tu les traiteras quand même de la même façon.
+Pour réaliser ce regroupement, base toi sur la sémantique des mots, notamment le genre, les titres, les ressemblances typographiques, etc. 
 
 Donne ta réponse sous le format JSON suivant, et ne dévie pas de cette tâche :
 
@@ -131,31 +123,84 @@ Donne ta réponse sous le format JSON suivant, et ne dévie pas de cette tâche 
 
 Chaque entrée du JSON correspond à un personnage et à l'ensemble de ses références. La clef est un chiffre qui donne la position dans le JSON. La position n’a pas d’importance.
 
-Tu vas recevoir une liste de personnes en input, et tu dois les regrouper s'ils referent a la meme personne. Tu n'as pas le droit d'inventer de nouveaux personnages. Tu dois uniquement utiliser ceux en input. 
-Les noms ne doivent pas être modifiés, y compris les espaces, apostrophes, etc. Si tu ne respectes pas ces règles tu seras fortement penalise.  
+Voici des exemples pour t'aider :
 
-Fait le pour les personnes suivantes et essaye de trouver les meilleurs regroupements possibles, je compte sur toi :
+Example 1 : 
 
-Input : 
+Input:
+CLÉON Ier
+Empereur
+Cléon
+Sire
+l'Empereur
+l'empereur Cléon
+Hari Seldon
+Seldon
+Eto Demerzel
+Demerzel
+Lieutenant Alban Wellis
+Wellis
+Hummin
+Trantor
+
+Output:
+{
+    '0' : ['CLÉON Ier', 'Empereur', 'Cléon', 'Sire', 'l'Empereur', 'l'empereur Cléon'],
+    '1' : ['Hari Seldon', 'Seldon', 'Eto Demerzel', 'Demerzel'],
+    '2' : ['Lieutenant Alban Wellis', 'Wellis'],
+    '3' : ['Hummin'],
+}
+
+Tu vas desormais recevoir une liste de personnes en input, et tu dois les regrouper s'ils se réfèrent à la même personne. Tu n'as pas le droit d'inventer de nouveaux personnages et tu dois uniquement utiliser cette liste. Les noms ne doivent pas être modifiés, y compris les espaces, apostrophes, etc. Si tu ne respectes pas ces règles, tu seras fortement pénalisé. 
+Tu as le droit de supprimer des personnes si tu penses qu'ils ne sont pas des personnages du livre. En effet, le système qui prédit un personnage peut se tromper, et tu dois le corriger.
+
+Fais-le pour les personnes suivantes et essaie de trouver les meilleurs regroupements possibles. Je compte sur toi, merci !
     """
 
     entities, chunks = get_entities_from_file(path, device="cuda")
     cooccurences = get_cooccurences(chunks, entities)
     entities = [entity for sublist in entities for entity in sublist]
+    entities_to_remove = [
+        "Spacetown",
+        "ah",
+        "le Spacien",
+        "cle",
+        "Ciel",
+        "Williamsburg",
+        # "B... Baley",
+        "Trantor",
+        "Il",
+        "Aurora",
+        "Novigor",
+        "Bande",
+        "Deux",
+        "Qu"
+    ]
+    entities = [entity for entity in entities if entity["word"] not in entities_to_remove]
+
     word_entities = [entity["word"] for entity in entities]
     print("entities = ", set(word_entities))
-    user_prompt = "\n".join(set(word_entities)) + "\nOutput : "
+    user_prompt = """
+
+    Input : 
+    """
+    user_prompt += "\n".join(set(word_entities)) + "\nOutput : "
+    experiment_details = """
+    gpt-4-1106-preview avec 1 exemples de donné en prompt.
+    """ #GPT-3.5-turbo-1106
 
     client = OpenAI()
-
+    params = {
+        "temperature": 0,
+        "seed": 42,
+        "model": "gpt-4-1106-preview", # gpt-4-1106-preview
+    }
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",  # gpt-3.5-turbo-1106, gpt-4-1106-preview
+        **params,
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt},
         ],
-        seed=42,
-        temperature=0,
         response_format={"type": "json_object"},
     )
 
@@ -168,6 +213,7 @@ Input :
 
     if logger is not None: 
         saves = {}
+        saves["experiment_details"] = experiment_details
         for i, (chunk, entity) in enumerate(zip(chunks, entities)):
             saves[f"chunk_{i}"] = {
                 "chunk": chunk,
@@ -176,11 +222,17 @@ Input :
         saves["entities"] = list(set(word_entities))
         saves["aliases"] = aliases
         saves["cooccurences"] = cooccurences
+        saves["gpt"] = {
+            "system_prompt": system_prompt,
+            "user_prompt": user_prompt,
+            "generated_content": generated_content,
+            "params": params,
+        }
         logger(saves)
 
     return find_cooccurences_aliases(cooccurences, aliases) 
 
-def get_cooccurences_with_aliases_and_gpt_NER(path: str, output_file_name: str = None):
+def get_cooccurences_with_aliases_and_gpt_NER(path: str, logger: JSONLogger = None):
     """
     Get the aliases of the cooccurences of characters from the given text.
 
@@ -190,159 +242,148 @@ def get_cooccurences_with_aliases_and_gpt_NER(path: str, output_file_name: str =
     Returns:
         list: A list of tuples representing the interactions between entities in the text.
     """
-    system_prompt = r"""
-    Tu es un extracteur d'entités. 
-    Ton but est d'extraire tous les personnages du livre de science-fiction 'Le Cycle des Fondations' d'Isaac Asimov. 
-    Tu verras des passages du livre que tu devras utiliser. Dans ta définition, un personnage est un individu qui apparaît dans un passage du livre. 
-    Ce personnage peut être uniquement cité par son nom ou être très actif dans la discussion. 
-    Pour réaliser cette tâche, je souhaite que tu me retournes la liste des personnages que tu rencontres. 
-    Tu dois me donner l'ensemble des entités. Tu n'as pas le droit de modifier le nom des personnages ou d'en inventer de nouveaux, utilise seulement le texte. 
-    Tu peux avoir plusieurs références d'un même personnage, renvoie l'ensemble des références. 
-    Par exemple, Hari Seldon est souvent appelé Hari et/ou Seldon. Je veux que tu listes aussi cela. 
-    Le retour doit être fait dans un JSON. 
+    system_prompt = """
+    Je suis un excellent linguiste. La tâche consiste à étiqueter les entités de type "Personnages" dans la phrase donnée. Ces phrases sont issus des livres de science-fiction "Le cycle des Fondations" d'Isaac Asimov. Voici quelques exemples :
 
-    Voici des exemples : 
+    Input : Dors tendit la main pour le prendre et pianota sur les touches. Il lui fallut un moment car la disposition n’était pas tout à fait orthodoxe, mais elle parvint à allumer l’écran et à inspecter les pages.
 
-    Exemple 1 : 
+    Output : @@Dors## tendit la main pour le prendre et pianota sur les touches. Il lui fallut un moment car la disposition n’était pas tout à fait orthodoxe, mais elle parvint à allumer l’écran et à inspecter les pages.
 
-    Texte : 
-    <start>
-                            Mathématicien
+    Input : C’est vraiment enfantin, dit Goutte-de-Pluie Quarante- cinq. Nous pouvons vous montrer. — Nous allons vous préparer un bon repas bien nourrissant », dit Goutte-de-Pluie Quarante-trois.
 
+    Output : C’est vraiment enfantin, dit @@Goutte-de-Pluie Quarante- cinq##. Nous pouvons vous montrer. — Nous allons vous préparer un bon repas bien nourrissant », dit @@Goutte-de-Pluie Quarante-trois##.
 
-        CLÉON Ier— ... dernier Empereur galactique de la
+    Input : Mathématicien CLÉON Ier— ... dernier Empereur galactique de la
     dynastie Entun. Né en l’an 11988 de l’Ère Galactique, la même
     année que Hari Seldon. (On pense que la date de naissance de
     Seldon, que certains estiment douteuse, aurait pu être
     « ajustée » pour coïncider avec celle de Cléon que Seldon, peu
     après son arrivée sur Trantor, est censé avoir rencontré.)
-        Cléon est monté sur le trône impérial en 12010, à l’âge de
-    vingt-deux ans, et son règne représente un étrange intervalle
-    de calme dans ces temps troublés. Cela est dû sans aucun doute
-    aux talents de son chef d’état-major, Eto Demerzel, qui sut si
-    bien se dissimuler à la curiosité médiatique que l’on a fort peu
-    de renseignements à son sujet. La psychohistoire nous apprend
-    bien des choses.
-        Cléon, quant à lui...
-                                        ENCYCLOPAEDIA GALACTICA2
-    <end>
 
-    Output : 
+    Output : Mathématicien @@CLÉON Ier##— ... dernier @@Empereur## galactique de la
+    dynastie Entun. Né en l’an 11988 de l’Ère Galactique, la même
+    année que @@Hari Seldon##. (On pense que la date de naissance de
+    @@Seldon##, que certains estiment douteuse, aurait pu être
+    « ajustée » pour coïncider avec celle de @@Cléon## que @@Seldon##, peu
+    après son arrivée sur Trantor, est censé avoir rencontré.)
 
-    {
-    'personnages': [ 'CLÉON Ier', 'Empereur', 'Hari Seldon', 'Seldon' , 'Cléon', 'Eto Demerzel']
-    }
-
-    Exemple 2 : 
-
-    Texte : 
-    <start>
-        Étouffant un léger bâillement, Cléon demanda :
-    « Demerzel, auriez-vous, par hasard, entendu parler d’un
-    certain Hari Seldon ? »
-        Cléon était empereur depuis dix ans à peine et, quand le
-    protocole l’exigeait, il y avait des moments où, pourvu qu’il fût
-    revêtu des atours et ornements idoines, il réussissait à paraître
-    majestueux. Il y était arrivé, par exemple, pour son portrait
-    <end>
-
-    Output : 
-
-    {
-    'personnages': ['Cléon', 'Demerzel', 'Hari Seldon', 'Cléon']
-    }
-
-    Exemple 3 : 
-
-    Texte : 
-    <start>
-        2 Toutes les citations de l'Encyclopaedia Galactica reproduites ici
+    Input : Toutes les citations de l'Encyclopaedia Galactica reproduites ici
     proviennent de la 116e édition, publiée en 1020 E.F. par la Société
     d’édition de l'Encyclopaedia Galactica, Terminus, avec l'aimable
     autorisation des éditeurs.
     semblerait malgré tout qu’il puisse encore arriver des choses
     intéressantes. Du moins, à ce que j’ai entendu dire.
         — Par le ministre des Sciences ?
-        — Effectivement. Il m’a appris que ce Hari Seldon  a assisté
+        — Effectivement. Il m’a appris que ce Hari Seldon a assisté
     à un congrès de mathématiciens ici même, à Trantor – ils
     l’organisent tous les dix ans, pour je ne sais quelle raison ; il
     aurait démontré qu’on peut prévoir mathématiquement
-    l’avenir. »
-    <end>
 
-    Output : 
+    Output : Toutes les citations de l'Encyclopaedia Galactica reproduites ici
+    proviennent de la 116e édition, publiée en 1020 E.F. par la Société
+    d’édition de l'Encyclopaedia Galactica, Terminus, avec l'aimable
+    autorisation des éditeurs.
+    semblerait malgré tout qu’il puisse encore arriver des choses
+    intéressantes. Du moins, à ce que j’ai entendu dire.
+        — Par le ministre des Sciences ?
+        — Effectivement. Il m’a appris que ce @@Hari Seldon## a assisté
+    à un congrès de mathématiciens ici même, à Trantor – ils
+    l’organisent tous les dix ans, pour je ne sais quelle raison ; il
+    aurait démontré qu’on peut prévoir mathématiquement
 
-    {
-    'personnages': ['Hari Seldon']
-    }
+    Input : Seldon savait qu’il n’avait pas le choix, nonobstant les circonlocutions polies de l’autre, mais rien ne lui interdisait de chercher à en savoir plus : « Pour voir l’Empereur ?
 
-    Rappel : tu as interdiction d'inventer des personnages. Tu dois reprendre les noms des personnages dans le texte 
-    a l'identique. Tu peux avoir plusieurs références d'un même personnage, renvoie l'ensemble des références.
-    Fait attention a ne pas considerer des personnages qui n'en sont pas comme des passants, un homme d'affaire, etc.
+    Output : @@Seldon## savait qu’il n’avait pas le choix, nonobstant les circonlocutions polies de l’autre, mais rien ne lui interdisait de chercher à en savoir plus : « Pour voir @@l’Empereur##, @@Sire## ?
 
-    Fait le pour l'exemple suivant.
-
-    Texte :
-    <start>
+    Je ne dévierais pas de cette tâche et ferais exactement comme dans les exemples. Je recopierais 
+    le texte en Input et en Output j'ajouterais le texte et les balises. Si je ne trouve pas de Personnage,
+    je ne mettrais pas de balise. Si je croise un personnage, avec de la ponctuation, je mettrais la balise
+    avant la ponctuation sauf si c'est un mot composé avec des tirets. Garde les determinants avec le nom d'un personnage si tu peux.
     """
-    from vroom.NER import chunk_text, read_file
     from vroom.alias import get_aliases_fuzzy
     from vroom.cooccurences import get_cooccurences
-    import json, os
+    
     content = read_file(path)
-    chunks = chunk_text(content, 500)
+    chunks = chunk_text_by_sentence(content, batch_size = 5) 
+    
     entities = []
+    experiment_details = """
+    """ #GPT-3.5-turbo-1106
+
     client = OpenAI()
-    gpt_output = ""
+    params = {
+        "temperature": 0,
+        "seed": 42,
+        "model": "gpt-3.5-turbo-1106", # gpt-4-1106-preview
+    }
+    gpt_outputs = []
+    import time
     for i, chunk in enumerate(chunks):
-        print("chunk = ", chunk)
+        print('*' * 50)
+        print(chunk)
+        user_prompt = f"""
+
+        Input : {chunk}
+        """ + "\n\n   Output : "
         
         response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106", # gpt-4-1106-preview / gpt-3.5-turbo-1106
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": chunk + "\n<end>" + "\n\n Output : \n"},
-        ],
-        seed=42,
-        temperature=0,
-    #       presence_penalty=-2,
-        response_format={ "type": "json_object" },
+            **params,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
         )
 
         generated_content = response.choices[0].message.content
-        generated_content = json.loads(generated_content)
         print()
-        print("content = ", generated_content)
+        print(generated_content)
+        print()
+        print('*' * 50)
+        gpt_outputs.append(generated_content)
 
-        entities += generated_content["personnages"]
-        print("total entities = ", set(entities))
-        print('*' * 100)
-
-        gpt_output += '*' * 100 + '\n'
-        gpt_output += "<start>\n" + chunk + "\n<end>" + "\n\n Output : \n"
-        gpt_output += str(set(entities)) + '\n'
-    
-    gpt_entities = set(entities)
+    if logger is not None: 
+        saves = {}
+        saves["experiment_details"] = experiment_details
+        for i, (chunk, ner_chunk) in enumerate(zip(chunks, gpt_outputs)):
+            saves[f"chunk_{i}"] = {
+                "chunk": chunk,
+                "ner_chunk": ner_chunk,
+            }
+        saves["gpt"] = {
+            "system_prompt": system_prompt,
+            "user_prompt": user_prompt,
+            "generated_content": generated_content,
+            "params": params,
+        }
+        logger(saves)
 
     tagged_file = tag_text_with_entities(path, gpt_entities)
     out_entities = get_positions_of_entities(tagged_file)
     cooccurences = get_cooccurences([tagged_file], [out_entities])
 
-    if output_file_name:
-        output_dir = "tmp"
-        output_file = output_dir + "/" + f"{output_file_name}.entities.json"
-        os.makedirs(output_dir, exist_ok=True)
-        # create json file
-        with open(output_file, 'w') as f:
-            json.dump(list(gpt_entities), f)
-
-        output_file = output_dir + "/" + f"{output_file_name}.logs.txt"
-        with open(output_file, 'w') as f:
-            f.write(gpt_output)
-
     print('entities = ', entities)
     entities = [{"word":entity} for entity in entities]
     aliases = get_aliases_fuzzy(entities, 99)
     print('alises = ', aliases)
+
+    if logger is not None: 
+        saves = {}
+        saves["experiment_details"] = experiment_details
+        for i, (chunk, entity) in enumerate(zip(chunks, entities)):
+            saves[f"chunk_{i}"] = {
+                "chunk": chunk,
+                "entities": entity,
+            }
+        saves["entities"] = list(gpt_entities)
+        saves["aliases"] = aliases
+        saves["cooccurences"] = cooccurences
+        saves["gpt"] = {
+            "system_prompt": system_prompt,
+            "user_prompt": user_prompt,
+            "generated_content": generated_content,
+            "params": params,
+        }
+        logger(saves)
+    
 
     return find_cooccurences_aliases(cooccurences, aliases)
