bc75e3b485bd6c173dc1f7df4861019b9f8bd492
GabrielDesbouis <90773358+GabrielDesbouis@users.noreply.github.com>
2024-01-08 20:05:05 +0100

    Docstring GPT Tokenizer

--- vroom/GPTTokenizer.py
+++ vroom/GPTTokenizer.py
@@ -1,16 +1,39 @@
+r"""  Package for tokenization tasks.
+
+Authors
+--------
+ * Gabriel DESBOUIS 2023
+"""
+
 import tiktoken
 
 
 class GPTTokenizer:
-    def __init__(self, model: str = "gpt-4"):
+    """
+    A wrapper for the GPT tokenizer.
+    It allows to tokenize a text and count the number of tokens.
+    """
+    def __init__(self, model: str = "gpt-4") -> None:
         self.encoding = tiktoken.encoding_for_model(model)
 
-    def tokenize(self, text: str):
-        self.tokenized = True
+    def tokenize(self, text: str) -> list[int]:
+        """
+        Tokenizes the given text.
+        Args:
+            text (str): The text to tokenize.
+        Returns:
+            list[int]: The list of tokens.
+        """
+        self.tokenized: bool = True
         self.tokens = self.encoding.encode(text)
         return self.tokens
 
-    def count_tokens(self):
+    def count_tokens(self) -> int:
+        """
+        Counts the number of tokens in the text.
+        Returns:
+            int: The number of tokens in the text.
+        """
         if not self.tokenized:
             raise Exception(
                 "You must tokenize the text before counting the tokens."
